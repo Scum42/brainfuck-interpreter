fb8df0546b28a2848d5f451a15d712299e28a047
Anthony Libardi <libardi42@gmail.com>
2017-01-12 18:23:36 -0500

    [ADD] Execution and memory visualization are complete!

---
+++ loopPair.h
@@ -0,0 +1,9 @@
+#pragma once
+
+int lastID = -1;
+
+struct LoopPair
+{
+    int id = -1;
+    unsigned int instruction;
+};
--- main.cpp
+++ main.cpp
@@ -1,27 +1,66 @@
+////////////////////////////////////////////////////////
+
 #include <iostream>
+#include <iomanip>
 #include <map>
 
 #include "args.h"
 #include "commands.h"
+#include "loopPair.h"
 
 using namespace std;
-map<int, char> memory;
+
+////////////////////////////////////////////////////////
+
+#pragma region PROTOTYPES
 
 string trim(string script);
-bool execute(char cmd);
+void execute();
+void validate();
+void validate(int pos);
+unsigned int match(bool forward);
+void memdump();
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region GLOBAL MEMORY
+
+int pointer = 0;
+map<int, char> memory;
+
+string script = "";
+unsigned int currentInstruction = 0;
+
+int memlower = 0;
+int memupper = 0;
+
+vector<LoopPair> loopStarts;
+vector<LoopPair> loopEnds;
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region MAIN
 
 int main(int argc, char** argv)
 {
     vector<string> args = convertArgs(argc, argv);
-    for each (string s in args) cout << s << endl;
+    //for each (string s in args) cout << s << endl;
 
-    string script = trim(args[1]);
-    cout << endl << script << endl;
+    script = trim(args[1]);
+    execute();
 
-    cin.get();
+    //cin.get();
 }
 
-///// TRIM /////////////////////////////////////////////////////
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region TRIM
 
 string trim(string script)
 {
@@ -29,7 +68,7 @@ string trim(string script)
     for each (char c in script)
     {
         bool isCmd = false;
-        
+
         for (int i = 0; i < NUM_COMMANDS; i++)
             if (c == COMMAND_CHARS[i])
                 isCmd = true;
@@ -40,9 +79,202 @@ string trim(string script)
     return result;
 }
 
-///// EXECUTE //////////////////////////////////////////////////
+#pragma endregion
 
-bool execute(char cmd)
+////////////////////////////////////////////////////////
+
+#pragma region VALIDATE
+
+void validate() { validate(pointer); }
+void validate(int pos)
 {
-    return true;
+    if (memory.find(pos) == memory.end())
+    {
+        memory[pos] = 0;
+    }
 }
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region MATCH
+
+unsigned int match(bool forward)
+{
+    // See if this bracket is matched; return the matching instruction
+    vector<LoopPair>& mylist = forward ? loopStarts : loopEnds;
+    vector<LoopPair>& matchlist = forward ? loopEnds : loopStarts;
+    for each (LoopPair lp in mylist)
+    {
+        if (lp.instruction == currentInstruction)
+        {
+            for each (LoopPair lp2 in matchlist)
+            {
+                if (lp.id == lp2.id)
+                {
+                    return lp2.instruction;
+                }
+            }
+        }
+    }
+
+    // Set up my pair
+    LoopPair myPair;
+    lastID++;
+    myPair.id = lastID;
+    myPair.instruction = currentInstruction;
+
+    // Match it
+    int count = 1;
+    int matchingInstruction = currentInstruction;
+    bool couldFind = true;
+    while (count > 0)
+    {
+        if (forward)
+        {
+            matchingInstruction++;
+            if (matchingInstruction >= script.length())
+            {
+                couldFind = false;
+                break;
+            }
+
+            if (script[matchingInstruction] == COMMAND_CHARS[Endloop]) count--;
+            else if (script[matchingInstruction] == COMMAND_CHARS[Loop]) count++;
+        }
+        else
+        {
+            matchingInstruction--;
+            if (matchingInstruction < 0)
+            {
+                couldFind = false;
+                break;
+            }
+
+            if (script[matchingInstruction] == COMMAND_CHARS[Endloop]) count++;
+            else if (script[matchingInstruction] == COMMAND_CHARS[Loop]) count--;
+        }
+    }
+
+    // Quit if the command is unmatched
+    if (!couldFind)
+    {
+        cout << "FATAL ERROR: Unmatched loop command found at position " << currentInstruction << endl;
+        exit(1);
+    }
+
+    // Add the entries for the commands, then return the matching instruction
+    LoopPair otherPair;
+    otherPair.id = lastID;
+    otherPair.instruction = matchingInstruction;
+
+    mylist.push_back(myPair);
+    matchlist.push_back(otherPair);
+
+    return otherPair.instruction;
+}
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region EXECUTE
+
+void execute()
+{
+    //cout << "/// OUTPUT ///\n";
+    for (; currentInstruction < script.length(); currentInstruction++)
+    {
+        char cmd = script[currentInstruction];
+
+        Command command = NOT_A_COMMAND;
+
+        for (int j = 0; j < NUM_COMMANDS; j++)
+        {
+            if (cmd == COMMAND_CHARS[j])
+            {
+                command = static_cast<Command>(j);
+                break;
+            }
+        }
+
+        if (command == NOT_A_COMMAND) return;
+
+        switch (command)
+        {
+            case Left:
+            {
+                pointer--;
+                if (pointer < memlower) memlower = pointer;
+                break;
+            }
+            case Right:
+            {
+                pointer++;
+                if (pointer > memupper) memupper = pointer;
+                break;
+            }
+            case Inc:
+            {
+                validate();
+                memory[pointer]++;
+                break;
+            }
+            case Dec:
+            {
+                validate();
+                memory[pointer]--;
+                break;
+            }
+            case Loop:
+            {
+                unsigned int matchingInstruction = match(true);
+                validate();
+                if (memory[pointer] == 0) currentInstruction = matchingInstruction;
+                break;
+            }
+            case Endloop:
+            {
+                unsigned int matchingInstruction = match(false);
+                validate();
+                if (memory[pointer] != 0) currentInstruction = matchingInstruction;
+                break;
+            }
+            case Get:
+            {
+                break;
+            }
+            case Put:
+            {
+                cout << memory[pointer];
+                break;
+            }
+        }
+
+        //memdump();
+    }
+
+    memdump();
+}
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
+
+#pragma region MEMDUMP
+
+void memdump()
+{
+    cout << "\nMEMORY: ";
+    for (int i = memlower; i <= memupper; i++)
+    {
+        validate(i);
+        cout << "[ " << left << setw(4) << static_cast<int>(memory[i]) << " ]";
+    }
+    cout << endl;
+}
+
+#pragma endregion
+
+////////////////////////////////////////////////////////
