2e4cf86c12a37cb271b1df5c733293e26d854d6b
Anthony Libardi <libardi42@gmail.com>
2017-01-12 18:28:11 -0500

    [ADD] Execution and memory visualization are complete!

--- main.cpp
+++ main.cpp
@@ -184,6 +184,7 @@ unsigned int match(bool forward)
 void execute()
 {
     //cout << "/// OUTPUT ///\n";
+    cout << endl;
     for (; currentInstruction < script.length(); currentInstruction++)
     {
         char cmd = script[currentInstruction];
