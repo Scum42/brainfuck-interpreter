e7621c5d3120a94afc478c006e000faac0189e84
Anthony Libardi <libardi42@gmail.com>
2017-01-12 15:17:59 -0500

    [ADD] Trim function implemented. Exec is in but a stub.

---
+++ commands.h
@@ -0,0 +1,29 @@
+#pragma once
+
+enum Command
+{
+    NOT_A_COMMAND = -1,
+
+    Left,
+    Right,
+    Inc,
+    Dec,
+    Get,
+    Put,
+    Loop,
+    Endloop,
+
+    NUM_COMMANDS
+};
+
+const char COMMAND_CHARS[NUM_COMMANDS] =
+{
+    '<',
+    '>',
+    '+',
+    '-',
+    ',',
+    '.',
+    '[',
+    ']'
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#include <map>
+
 #include "args.h"
+#include "commands.h"
 
 using namespace std;
+map<int, char> memory;
+
+string trim(string script);
+bool execute(char cmd);
 
 int main(int argc, char** argv)
 {
     vector<string> args = convertArgs(argc, argv);
-
     for each (string s in args) cout << s << endl;
 
-    cin.get();
+    string script = trim(args[1]);
+    cout << endl << script << endl;
+
+    system("pause");
+}
+
+///// TRIM /////////////////////////////////////////////////////
+
+string trim(string script)
+{
+    string result = "";
+    for each (char c in script)
+    {
+        bool isCmd = false;
+        
+        for (int i = 0; i < NUM_COMMANDS; i++)
+            if (c == COMMAND_CHARS[i])
+                isCmd = true;
+
+        if (isCmd) result += c;
+    }
+
+    return result;
+}
+
+///// EXECUTE //////////////////////////////////////////////////
+
+bool execute(char cmd)
+{
+    return true;
 }
